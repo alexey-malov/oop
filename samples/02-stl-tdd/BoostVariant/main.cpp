a5ea82274b6dfcf43f80c012deed4672187b48f6
Alexey Malov <alexey.malov@cpslabs.net>
2016-03-16 13:35:56 +0300

    Добавлен пример, использующий boost::variant

---
+++ samples/02-stl-tdd/BoostVariant/main.cpp
@@ -0,0 +1,76 @@
+﻿#include <boost/variant.hpp>
+#include <utility>
+#include <array>
+#include <iostream>
+
+typedef std::pair<double, double> TwoRoos;
+// С т.з. C++ структуры NoRealRoots и NotAQuadraticEquation являются разными типами
+// поэтому мы можем их использовать для того, чтобы отличить отсутствие корней от 
+// неквадратичности уравнения
+struct NoRealRoots{};
+struct NotAQuadraticEquation{};
+
+// Допустимые значения, которые может принимать решение квадратного уравнения
+// Хранится лишь одно из данных типов значений
+typedef boost::variant<NoRealRoots, double, TwoRoos, NotAQuadraticEquation> QuadraticEquationSolution;
+
+// Структура-наследник boost::static_visitor, выполняющая печать решения квадратного уравнения
+struct QuadraticEquationSolutionPrinter : public boost::static_visitor<void>
+{
+	// Сохраняем ссылку на поток вывода, что даст возможность вывода
+	// в произвольный поток-наследник ostream (ofstream, ostringstream и т.п.)
+	QuadraticEquationSolutionPrinter(std::ostream & strm)
+		:m_strm(strm)
+	{}
+
+	// Печать одного корня
+	void operator() (double x)
+	{
+		m_strm << x << std::endl;
+	}
+
+	// Печать двух корней
+	void operator() (TwoRoos roots)
+	{
+		m_strm << roots.first << "; " << roots.second << std::endl;
+	}
+
+	// Вывод результата "нет корней"
+	void operator() (NoRealRoots)
+	{
+		m_strm << "No real roots" << std::endl;
+	}
+
+	// Вывод ошибки (не квадратное уравнение)
+	void operator() (NotAQuadraticEquation)
+	{
+		m_strm << "Not a quadratic equation" << std::endl;
+	}
+private:
+	std::ostream & m_strm;
+};
+
+// Решатель квадратного уравнения
+QuadraticEquationSolution SolveQuadraticEquation(double a, double b, double c)
+{
+	// Напишите реализацию самостоятельно, возвращая одни из следующих результатов
+
+	return NotAQuadraticEquation();
+	return NoRealRoots();
+	return std::make_pair(3.17, 17.8); // Two roots
+	return 42.0; // one root
+}
+
+
+int main()
+{
+	auto result = SolveQuadraticEquation(1, 2, 3);
+
+	// Создаем объект, выполняющий печать в cout
+	QuadraticEquationSolutionPrinter printer(std::cout);
+	// Просим result вызвать оператор(), передав хранимое значение
+	result.apply_visitor(printer);
+
+
+	return 0;
+}
--- samples/02-stl-tdd/StringReference/main.cpp
+++ samples/02-stl-tdd/StringReference/main.cpp
@@ -1,4 +1,4 @@
-#include <boost/utility/string_ref.hpp>
+п»ї#include <boost/utility/string_ref.hpp>
 #include <iostream>
 #include <cassert>
 #include <chrono>
@@ -9,7 +9,7 @@ using boost::string_ref;
 int main()
 {
 	const string url = "http://long.domain.name.at.google.com/long.document.path.txt";
-	// Решение с использование substr
+	// Р РµС€РµРЅРёРµ СЃ РёСЃРїРѕР»СЊР·РѕРІР°РЅРёРµ substr
 
 
 	{
@@ -35,12 +35,12 @@ int main()
 		t = chrono::steady_clock().now().time_since_epoch() - t;
 		cout << "string operations duration " << t.count() << endl;
 	}
-	// аналогичный код, использующий string_ref
+	// Р°РЅР°Р»РѕРіРёС‡РЅС‹Р№ РєРѕРґ, РёСЃРїРѕР»СЊР·СѓСЋС‰РёР№ string_ref
 	{
 		auto t = chrono::steady_clock().now().time_since_epoch();
 		for (auto i = 0; i < 10000000; ++i)
 		{
-			string_ref urlRef(url); // string_ref, ссылающийся на всю строку
+			string_ref urlRef(url); // string_ref, СЃСЃС‹Р»Р°СЋС‰РёР№СЃСЏ РЅР° РІСЃСЋ СЃС‚СЂРѕРєСѓ
 
 			const string schemeDelimiter = "://";
 			auto schemeEnd = urlRef.find(schemeDelimiter);
@@ -48,7 +48,7 @@ int main()
 			auto scheme = urlRef.substr(0, schemeEnd);
 			assert(scheme == "http");
 
-			urlRef = urlRef.substr(schemeEnd + scheme.length() - 1); // отбросили схему с разделителем
+			urlRef = urlRef.substr(schemeEnd + scheme.length() - 1); // РѕС‚Р±СЂРѕСЃРёР»Рё СЃС…РµРјСѓ СЃ СЂР°Р·РґРµР»РёС‚РµР»РµРј
 			auto domainEnd = urlRef.find('/');
 			assert(domainEnd != string_ref::npos);
 			auto domain = urlRef.substr(0, domainEnd);
