dc6a963a7552634896a42165014139852f81c200
Alexey Malov <alexey.malov@cpslabs.net>
2016-05-18 11:46:41 +0300

    Частично реализован метод Append

--- labs/lab7/task2/MyArray.h
+++ labs/lab7/task2/MyArray.h
@@ -1,9 +1,46 @@
 #pragma once
 
+#include <new>
+#include <algorithm>
+
 template <typename T>
 class CMyArray
 {
 public:
+	void Append(const T & value)
+	{
+		if (m_end == m_endOfCapacity) // no free space
+		{
+			size_t newCapacity = std::max(1u, GetCapacity() * 2);
+
+			auto newBegin = RawAlloc(newCapacity);
+			T *newEnd = newBegin;
+			try
+			{
+				CopyItems(m_begin, m_end, newBegin, newEnd);
+				// Конструируем копию value по адресу newItemLocation
+				new (newEnd)T(value);
+				++newEnd;
+			}
+			catch (...)
+			{
+				DeleteItems(newBegin, newEnd);
+				throw;
+			}
+			DeleteItems(m_begin, m_end);
+
+			// Переключаемся на использование нового хранилища элементов
+			m_begin = newBegin;
+			m_end = newEnd;
+			m_endOfCapacity = m_begin + newCapacity;
+
+		}
+		else // has free space
+		{
+
+		}
+	}
+
 	size_t GetSize()const
 	{
 		return m_end - m_begin;
@@ -13,6 +50,60 @@ public:
 	{
 		return m_endOfCapacity - m_begin;
 	}
+	~CMyArray()
+	{
+		DeleteItems(m_begin, m_end);
+	}
+private:
+
+	static void DeleteItems(T *begin, T *end)
+	{
+		// Разрушаем старые элементы
+		DestroyItems(begin, end);
+		// Освобождаем область памяти для их хранения
+		RawDealloc(begin);
+	}
+	// Копирует элементы из текущего вектора в to, возвращает newEnd
+	static void CopyItems(T *begin, T *end, T * const dstBegin, T * & dstEnd)
+	{
+		for (dstEnd = dstBegin; begin != end; ++begin, ++dstEnd)
+		{
+			// Construct "T" at "dstEnd" as a copy of "*begin"
+			new (dstEnd)T(*begin);
+		}
+	}
+
+	static void DestroyItems(T *from, T *to)
+	{
+		if (from == nullptr)
+		{
+			return;
+		}
+		// dst - адрес объект, при конструирование которого было выброшено исключение
+		// to - первый скорнструированный объект
+		while (--to >= from)
+		{
+			// явно вызываем деструктор для шаблонного типа T
+			to->~T();
+		}
+	}
+
+	static T *RawAlloc(size_t n)
+	{
+		size_t memSize = n * sizeof(T);
+		T *p = static_cast<T*>(malloc(memSize));
+		if (!p)
+		{
+			throw std::bad_alloc();
+		}
+		return p;
+	}
+
+	static void RawDealloc(T *p)
+	{
+		free(p);
+	}
+
 private:
 	T *m_begin = nullptr;
 	T *m_end = nullptr;
--- labs/lab7/task2/MyArrayTests.cpp
+++ labs/lab7/task2/MyArrayTests.cpp
@@ -14,12 +14,24 @@ struct EmptyStringArray
 };
 
 BOOST_FIXTURE_TEST_SUITE(MyArray, EmptyStringArray)
-	BOOST_AUTO_TEST_CASE(is_empty_by_default)
-	{
-		BOOST_CHECK_EQUAL(arr.GetSize(), 0u);
-	}
-	BOOST_AUTO_TEST_CASE(has_0_capacity)
-	{
-		BOOST_CHECK_EQUAL(arr.GetCapacity(), 0u);
-	}
+	BOOST_AUTO_TEST_SUITE(by_default)
+		BOOST_AUTO_TEST_CASE(is_empty)
+		{
+			BOOST_CHECK_EQUAL(arr.GetSize(), 0u);
+		}
+		BOOST_AUTO_TEST_CASE(has_0_capacity)
+		{
+			BOOST_CHECK_EQUAL(arr.GetCapacity(), 0u);
+		}
+	BOOST_AUTO_TEST_SUITE_END()
+
+	BOOST_AUTO_TEST_SUITE(after_appending_an_item)
+		BOOST_AUTO_TEST_CASE(increases_its_size)
+		{
+			arr.Append(ArrayItem());
+			BOOST_CHECK_EQUAL(arr.GetSize(), 1u);
+			arr.Append(ArrayItem());
+			BOOST_CHECK_EQUAL(arr.GetSize(), 2u);
+		}
+	BOOST_AUTO_TEST_SUITE_END()
 BOOST_AUTO_TEST_SUITE_END()
