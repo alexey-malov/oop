5449fac221734a223f07702b77d55b6d3536cb6d
Alexey Malov <alexey.malov@cpslabs.net>
2016-05-18 10:18:00 +0300

    Разработа каркас класса CMyArray и его тесты
    Массив изначально пуст

---
+++ labs/lab7/task2/MyArray.h
@@ -0,0 +1,11 @@
+#pragma once
+
+template <typename T>
+class CMyArray
+{
+public:
+	size_t GetSize()const
+	{
+		return 0;
+	}
+};
---
+++ labs/lab7/task2/MyArrayTests.cpp
@@ -0,0 +1,21 @@
+#include "stdafx.h"
+#include "MyArray.h"
+
+using namespace std;
+
+struct ArrayItem
+{
+
+};
+
+struct EmptyStringArray
+{
+	CMyArray<ArrayItem> arr;
+};
+
+BOOST_FIXTURE_TEST_SUITE(MyArray, EmptyStringArray)
+	BOOST_AUTO_TEST_CASE(is_empty_by_default)
+	{
+		BOOST_CHECK_EQUAL(arr.GetSize(), 0u);
+	}
+BOOST_AUTO_TEST_SUITE_END()
