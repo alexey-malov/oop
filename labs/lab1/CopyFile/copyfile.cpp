7f0426b19586a0284339b9587ae2425e28b4a3d8
Alexey Malov <alexey.malov@cpslabs.net>
2016-09-08 23:02:22 +0300

    Реализовано копирование файла

--- labs/lab1/CopyFile/copyfile.cpp
+++ labs/lab1/CopyFile/copyfile.cpp
@@ -1,4 +1,5 @@
-#include <iostream>
+﻿#include <iostream>
+#include <fstream>
 
 using namespace std;
 
@@ -11,6 +12,44 @@ int main(int argc, char * argv[])
 		return 1;
 	}
 
-	cout << argv[1] << "\n" << argv[2];
+	// Объявили переменную типа ifstream 
+	// (input file stream, поток для чтения из файла), проинициализировав его
+	// именем входного файла
+	ifstream input(argv[1]);
+
+	// вызываем метод is_open() у объекта input,
+	// который вернет true, если файл был открыт
+	if (!input.is_open()) 
+	{
+		cout << "Failed to open " << argv[1] << " for reading\n";
+		return 1;
+ 	}
+
+	// создали поток для записи в выходной файл
+	ofstream output(argv[2]);
+	if (!output.is_open())
+	{
+		cout << "Failed to open " << argv[2] << " for writing\n";
+		return 1;
+	}
+
+	char ch;
+	// Считываем в переменную ch символ из входного потока
+	// возвращаем значение, приводимое к bool, которое сигнализирует об успехе
+	while (input.get(ch))
+	{
+		if (!output.put(ch))
+		{
+			cout << "Failed to save data on disk\n";
+			return 1;
+		}
+	}
+
+	if (!output.flush()) // Если не удалось сбросить данные на диск
+	{
+		cout << "Failed to save data on disk\n";
+		return 1;
+	}
+
 	return 0;
 }
