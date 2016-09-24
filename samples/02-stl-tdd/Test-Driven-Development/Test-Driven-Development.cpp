16a502d8bdb5b2b2242743e95afe2a969577afa2
Alexey Malov <alexey.malov@cpslabs.net>
2016-09-24 11:55:43 +0300

    поправил warning-и компиляции

--- samples/02-stl-tdd/Test-Driven-Development/Test-Driven-Development.cpp
+++ samples/02-stl-tdd/Test-Driven-Development/Test-Driven-Development.cpp
@@ -21,9 +21,9 @@ private:
 	void test_unit_finish(std::ostream &os,
 		boost::unit_test::test_unit const& tu, unsigned long elapsed)
 	{
-		elapsed;
-		tu;
-		os;
+		(void)&elapsed;
+		(void)&tu;
+		(void)&os;
 		m_indent -= 2;
 	}
 	int m_indent;
