d187d2002f26699d303e3a6cd70016e018a1e691
Alexey Malov <alexey.malov@cpslabs.net>
2016-09-28 11:07:51 +0300

    Добавлен пример, иллюстрирующий возврат потока из функции

---
+++ samples/01-basics/stream-moving/main.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+ifstream OpenFileForReading(const string & fileName)
+{
+	ifstream strm(fileName);
+	return move(strm); // Нельзя вернуть копию потока, но можно переместить объект потока
+}
+
+int main()
+{
+	if (auto strm = OpenFileForReading("Text.txt"))
+	{
+		string line;
+		while (getline(strm, line))
+		{
+			cout << line << endl;
+		}
+	}
+	else
+	{
+		cout << "Failed to open Text.txt" << endl;
+	}
+}
