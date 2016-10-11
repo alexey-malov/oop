e1517c472c687f14d4aaa158f2c2a15fadd07864
Alexey Malov <alexey.malov@cpslabs.net>
2016-10-11 20:55:03 +0300

    Добавлены конфигурации для 64-битной платформы, а также заготовки тестов

--- labs/lab3/Task1Tests/RemoteControlTests.cpp
+++ labs/lab3/Task1Tests/RemoteControlTests.cpp
@@ -26,6 +26,8 @@ struct RemoteControlFixture : RemoteControlDependencies
 	{
 	}
 
+	// Вспомогательная функция для проверки работы команды command
+	// Она принимает ожидаемый номер канала expectedChannel и expectedOutput
 	void VerifyCommandHandling(const string& command, const optional<int> & expectedChannel, const string& expectedOutput)
 	{
 		input << command;
@@ -49,5 +51,69 @@ BOOST_FIXTURE_TEST_SUITE(Remote_Control, RemoteControlFixture)
 		tv.TurnOn();
 		VerifyCommandHandling("TurnOff", none, "TV is turned off\n");
 	}
+
+/*
+	// Раскомментируйте тест, проверяющий работу команды Info
+	// Убедитесь, что он не проходит (т.к. в CRemoteControl отсутствует требуемый функционал)
+	// Доработайте простейшим образом класс CRemoteControl, чтобы этот тест и предыдущие проходили
+	// При необходимости выполните рефакторинг кода, сохраняя работоспособность тестов
+	BOOST_AUTO_TEST_CASE(can_print_tv_info)
+	{
+		// Ожидаемое поведение команды Info, вызванной у выключенного телевизора
+		VerifyCommandHandling("Info", none, "TV is turned off\n");
+
+		// Проверяем поведение команды Info у включенного телевизора
+		tv.TurnOn();
+		tv.SelectChannel(42);
+		VerifyCommandHandling("Info", 42, "TV is turned on\nChannel is: 42\n");
+	}
+*/
+
+/*
+	// Раскомментируйте тест, проверяющий работу команды SelectChannel 
+	//	попытке при выбрать доступный номер канала у включенного телевизора
+	// Убедитесь, что он не проходит (т.к. в CRemoteControl отсутствует требуемый функционал)
+	// Доработайте простейшим образом класс CRemoteControl, чтобы этот тест и предыдущие проходили
+	// При необходимости выполните рефакторинг кода, сохраняя работоспособность тестов
+	BOOST_AUTO_TEST_CASE(can_select_a_valid_channel_when_tv_which_is_on)
+	{
+		tv.TurnOn();
+		VerifyCommandHandling("SelectChannel 42", 42, "Channel selected\n");
+	}
+*/
+
+/*
+	// Раскомментируйте тест, проверяющий работу команды SelectChannel 
+	//	попытке при выбрать доступный либо недоступный номер канала у выключенного телевизора
+	// Убедитесь, что он не проходит (т.к. в CRemoteControl отсутствует требуемый функционал)
+	// Доработайте простейшим образом класс CRemoteControl, чтобы этот тест и предыдущие проходили
+	// При необходимости выполните рефакторинг кода, сохраняя работоспособность тестов
+	BOOST_AUTO_TEST_CASE(cant_select_channel_when_tv_is_turned_off)
+	{
+		VerifyCommandHandling("SelectChannel 42", none, "Can't select channel because TV is turned off\n");
+		VerifyCommandHandling("SelectChannel 100", none, "Can't select channel because TV is turned off\n");
+	}
+*/
+
+/*
+	// Раскомментируйте тест, проверяющий работу команды SelectChannel 
+	//	попытке при выбрать недоступный номер канала у включенного телевизора
+	// Убедитесь, что он не проходит (т.к. в CRemoteControl отсутствует требуемый функционал)
+	// Доработайте простейшим образом класс CRemoteControl, чтобы этот тест и предыдущие проходили
+	// При необходимости выполните рефакторинг кода, сохраняя работоспособность тестов
+	BOOST_AUTO_TEST_CASE(cant_select_an_invalid_channel_when_tv_is_on)
+	{
+		tv.TurnOn();
+		tv.SelectChannel(42);
+		VerifyCommandHandling("SelectChannel 100", 42, "Invalid channel\n");
+		VerifyCommandHandling("SelectChannel 0", 42, "Invalid channel\n");
+	}
+*/
+
+	// Напишите тесты для недостающего функционала класса CRemoteControl (если нужно)
+	//	и для дополнительных заданий на бонусные баллы (если нужно)
+	// После написания каждого теста убедитесь, что он не проходит.
+	// Доработайте простейшим образом класс CRemoteControl, чтобы этот тест и предыдущие проходили
+	// При необходимости выполните рефакторинг кода, сохраняя работоспособность тестов
 
 BOOST_AUTO_TEST_SUITE_END()
