5386f4a8d5780a1807687cabacb7cc8a862a2850
Alexey Malov <alexey.malov@cpslabs.net>
2016-10-27 20:16:21 +0300

    Доработан пример, иллюстрирующий использование наследования и интерфейсов

--- samples/04-inheritance/polymorphysm/polymorphysm.cpp
+++ samples/04-inheritance/polymorphysm/polymorphysm.cpp
@@ -2,6 +2,7 @@
 //
 
 #include "stdafx.h"
+#include <string>
 
 using namespace std;
 
@@ -10,6 +11,23 @@ class ISerializable
 public:
 	virtual void Serialize(ostream & strm)const = 0;
 	virtual ~ISerializable(){}
+};
+
+class ICanvas
+{
+public:
+	virtual void DrawCircle(double x, double y, double radius, const string & color) = 0;
+	virtual void DrawLine(double x0, double y0, double x1, double y1, const string& color) = 0;
+
+	virtual ~ICanvas() = default;
+};
+
+class IDrawable
+{
+public:
+	virtual void Draw(ICanvas & canvas) const = 0;
+
+	virtual ~IDrawable() = default;
 };
 
 class CCat : public ISerializable
@@ -21,7 +39,9 @@ public:
 	}
 };
 
-class CShape : public ISerializable
+class CShape 
+	: public ISerializable
+	, public IDrawable
 {
 public:
 	virtual double GetArea()const = 0;
@@ -38,7 +58,14 @@ public:
 	{
 		return M_PI * m_radius * m_radius;
 	}
+	void Draw(ICanvas & canvas) const override
+	{
+		canvas.DrawCircle(m_centerX, m_centerY, m_radius, "red");
+	}
+
 private:
+	int m_centerX = 100;
+	int m_centerY = 100;
 	int m_radius = 42;
 };
 
@@ -59,18 +86,52 @@ void Serialize(vector<const ISerializable*> const & someSerializableObjects, ost
 	}
 }
 
+class CLoggingCanvas : public ICanvas
+{
+public:
+
+	void DrawCircle(double x, double y, double radius, const string & color) override
+	{
+		cout << "DrawCircle: " << x << ", " << y << ", " << radius << ", " << color << endl;
+	}
+
+	void DrawLine(double x0, double y0, double x1, double y1, const string& color) override
+	{
+		cout << "DrawLine: (" << x0 << ", " << y0 << ") - (" 
+			<< x1 << ", " << y1 << "), " << color << endl;
+	}
+};
+
+void DrawPicture(
+	vector<const IDrawable*> const & drawables,
+	ICanvas & canvas
+	)
+{
+	for (auto drawable : drawables)
+	{
+		drawable->Draw(canvas);
+	}
+}
+
 int _tmain(int argc, _TCHAR* argv[])
 {
 	CCircle circle;
 	CCat cat;
 	DoSomethingWithShape(circle);
+
 	cout << "----" << endl;
+
 	vector<const ISerializable*> objects = { &cat, &circle };
 	Serialize(objects, cout);
 	ostringstream strm;
 	Serialize(objects, strm);
 	cout << "!!!" << strm.str() << "!!!" << endl;
 
+	cout << "----" << endl;
+
+	vector<const IDrawable *> picture = { &circle, };
+	CLoggingCanvas canvas;
+	DrawPicture(picture, canvas);
 
 	return 0;
 }
