6bfdd9963d13ab76b279a339ce38ccbcc5a14c05
Alexey Malov <malov.aleksey@gmail.com>
2018-02-09 22:05:16 +0300

    Добавлена проверка количества аргументов командной строки

--- labs/lab1/Replace/Replace.cpp
+++ labs/lab1/Replace/Replace.cpp
@@ -4,8 +4,14 @@
 #include "stdafx.h"
 
 
-int main()
+int main(int argc, char * argv[])
 {
+	if (argc != 5)
+	{
+		std::cout << "Invalid argument count\n"
+			<< "Usage: replace.exe <inputFile> <outputFile> <searchString> <replacementString>\n";
+		return 1;
+	}
     return 0;
 }
 
--- labs/lab1/Replace/test.bat
+++ labs/lab1/Replace/test.bat
@@ -4,4 +4,12 @@ rem %~1 - значение первого аргумента командной строки bat-файла с удалением обра
 rem Переменная PROGRAM будет хранить первый аргумент командной строки заключённый в кавычки
 set PROGRAM="%~1"
 
+%PROGRAM% > nul
+if NOT ERRORLEVEL 1 goto err
+
 echo OK
+exit 0
+
+:err
+echo Program testing failed
+exit 1
