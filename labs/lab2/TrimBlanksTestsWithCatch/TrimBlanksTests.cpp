7940718d9a40196d6190400c1515438939f93cbb
Alexey Malov <malov.aleksey@gmail.com>
2018-03-10 10:32:50 +0300

    Добавлен проект для тестирования TrimBlanks с использованием фреймворка Catch

---
+++ labs/lab2/TrimBlanksTestsWithCatch/TrimBlanksTests.cpp
@@ -0,0 +1,6 @@
+#include "stdafx.h"
+
+TEST_CASE("Just test")
+{
+	REQUIRE(2 + 2 == 4);
+}
---
+++ labs/lab2/TrimBlanksTestsWithCatch/stdafx.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+
+// Подключаем бибилиотеку Catch (без генерирования функции main)
+
+#include "../../../catch/catch.hpp"
+
