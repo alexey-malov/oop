bea97c5cd6944b7a88e38044c8f5d6744957feec
Alexey Malov <malov.aleksey@gmail.com>
2018-05-18 21:46:23 +0300

    Добавлен пример, иллюстрирующий порочность использования using namespace в заголовочных файлах

---
+++ samples/02-stl-tdd/NeverWriteUsingNamespaceInHeaders/Bad.h
@@ -0,0 +1,7 @@
+#pragma once
+#pragma warning(disable:4996)
+#include <memory>
+#include <iostream>
+#include <algorithm>
+
+using namespace std; // Don't do this!
---
+++ samples/02-stl-tdd/NeverWriteUsingNamespaceInHeaders/NeverWriteUsingNamespaceInHeaders.cpp
@@ -0,0 +1,20 @@
+﻿#include "Bad.h"
+
+// Программа не будет компилироваться, пока из Bad.h не убрать using namespace std; 
+// наша функция copy будет конфликтовать с алгоритмом std::copy 
+void copy(int* from, int* to, int* foo)
+{
+	if (foo != nullptr)
+	{
+		to = from;
+	}
+}
+
+int main()
+{
+	int x = 5;
+	int y = 6;
+	copy(&x, &y, nullptr);
+	std::cout << "y:" << y;
+	return 0;
+}
