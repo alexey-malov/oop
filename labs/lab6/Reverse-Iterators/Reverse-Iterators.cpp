2a803890b2253d500cd25acefb5a1bc64d0bcd35
Alexey Malov <malov.aleksey@gmail.com>
2018-05-25 20:48:41 +0300

    Добавлены примеры по безопасной обработке исключений и работе с реверсивными итераторами

---
+++ labs/lab6/Reverse-Iterators/Reverse-Iterators.cpp
@@ -0,0 +1,26 @@
+﻿// Reverse-Iterators.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	auto s = "Hello world!"s;
+
+	copy(s.begin(), s.end(), ostream_iterator<char>(cout, "|"));
+	cout << "\n";
+	copy(s.rbegin(), s.rend(), ostream_iterator<char>(cout, "|"));
+	cout << "\n";
+	copy(make_reverse_iterator(s.end()),
+		make_reverse_iterator(s.begin()),
+		ostream_iterator<char>(cout, "|"));
+	cout << "\n";
+
+	return 0;
+}
