667137e57bdaa7bf15aed9923ba898e0eab5c776
Alexey Malov <malov.aleksey@gmail.com>
2018-06-08 22:36:22 +0300

    AppendProperties сделан приватным

--- labs/lab4/bodies/Body.h
+++ labs/lab4/bodies/Body.h
@@ -2,17 +2,17 @@
 class CBody
 {
 public:
-	CBody(const std::string & type, double density);
-	
+	CBody(const std::string& type, double density);
+
 	double GetDensity() const;
 	virtual double GetVolume() const = 0;
 	double GetMass() const;
 	std::string ToString() const;
 
 	virtual ~CBody();
-protected:
-	virtual void AppendProperties(std::ostream & strm) const = 0;
+
 private:
+	virtual void AppendProperties(std::ostream& strm) const = 0;
 	double m_density;
 	std::string m_type;
 };
--- labs/lab4/bodies/Sphere.h
+++ labs/lab4/bodies/Sphere.h
@@ -1,16 +1,15 @@
 #pragma once
 #include "Body.h"
 
-class CSphere final :
-	public CBody
+class CSphere final : public CBody
 {
 public:
 	CSphere(double density, double radius);
 
 	double GetRadius() const;
 	double GetVolume() const override;
-protected:
-	void AppendProperties(std::ostream & strm) const override;
+
 private:
-	double m_radius;	
+	void AppendProperties(std::ostream& strm) const override;
+	double m_radius;
 };
