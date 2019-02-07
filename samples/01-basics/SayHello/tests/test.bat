f702d1b1141c64a7edde25c3cc878999f5b57cbb
Alexey Malov <malov.aleksey@gmail.com>
2019-02-07 22:39:47 +0300

    Тесты программы SayHello перемещены к каталог tests

--- samples/01-basics/SayHello/tests/test.bat
+++ samples/01-basics/SayHello/tests/test.bat
@@ -4,22 +4,23 @@
 :: В переменную PROGRAM заносим путь к программе
 set PROGRAM="%~1"
 
+
 :: Путь к файлу с выходными данными программы
 set OUT="%TEMP%\out.txt"
 
 :: Тестируем имя из одного слова
-%PROGRAM% < Boris.txt > %OUT% || goto err
+%PROGRAM% < "%~dp0Boris.txt" > %OUT% || goto err
 :: Сравниваем содержимое файла с выводом программы и эталон
 :: если они не равны, переходим на строку err
-fc %OUT% Boris-out.txt || goto err
+fc %OUT% "%~dp0Boris-out.txt" || goto err
 
 :: Тестируем имя из нескольких слов
-%PROGRAM% < HarryPotter.txt > %OUT% || goto err
-fc %OUT% HarryPotter-out.txt || goto err
+%PROGRAM% < "%~dp0HarryPotter.txt" > %OUT% || goto err
+fc %OUT% "%~dp0HarryPotter-out.txt" || goto err
 
 :: Тестируем пустое имя
-%PROGRAM% < Empty.txt > %OUT% || goto err
-fc %OUT% Empty-out.txt || goto err
+%PROGRAM% < "%~dp0Empty.txt" > %OUT% || goto err
+fc %OUT% "%~dp0Empty-out.txt" || goto err
 
 echo All tests passed
 exit /B 0
