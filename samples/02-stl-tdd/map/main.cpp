7a68f9ba37723b5e83252803f1c4f94c6726f224
Alexey Malov <alexey.malov@ispringsolutions.com>
2021-02-11 18:09:41 +0300

    Обновлены примеры и лекция

--- samples/02-stl-tdd/map/main.cpp
+++ samples/02-stl-tdd/map/main.cpp
@@ -1,11 +1,11 @@
-﻿#include <map>
-#include <unordered_map>
+﻿#include <iostream>
+#include <map>
 #include <string>
-#include <iostream>
+#include <unordered_map>
 
 using namespace std;
 
-void main()
+void CountChars()
 {
 	map<char, int> charFreq;
 	//Можно использовать unordered_map
@@ -16,24 +16,71 @@ void main()
 		if (isalpha(ch))
 		{
 			++charFreq[ch];
-			//auto it = charFreq.find(ch);
-			//if (it != charFreq.end()) // Найдено
-			//{
-			//	++it->second;
-			//}
-			//else
-			//{
-			//	//charFreq.insert(pair<char, int>(ch, 1));
-			//	//charFreq.insert(make_pair(ch, 1));
-			//	charFreq.emplace(ch, 1);
-			//}
 		}
 	}
 
-	for (auto & chInfo : charFreq)
+	for (auto& chInfo : charFreq)
 	{
 		cout << chInfo.first << ": " << chInfo.second << endl;
 	}
 
 	cout << endl;
+}
+
+void AnimalLegs()
+{
+	map<string, int> legsCount = {
+		{ "dog"s, 4 }, { "sparrow"s, 2 }, { "spider"s, 8 },
+		{ "octopus"s, 8 }, { "ant"s, 6 }
+	};
+
+	for (const auto& [name, legs] : legsCount)
+	{
+		cout << name << " has " << legs << " legs" << endl;
+	}
+
+	cout << "Enter animal name: ";
+	string animalName;
+	getline(cin, animalName);
+	if (auto it = legsCount.find(animalName);
+		it != legsCount.end())
+	{
+		cout << animalName << " has " << it->second << " legs" << endl;
+	}
+	else
+	{
+		cout << "No info about " << animalName << endl;
+	}
+}
+
+map<char, int> BuildCharCounters(const string& word)
+{
+	map<char, int> counters;
+	for (char ch : word)
+	{
+		++counters[ch];
+	}
+	return counters;
+}
+
+void Anagrams()
+{
+	cout << "Enter two words: ";
+	string word1, word2;
+	cin >> word1 >> word2;
+	if (BuildCharCounters(word1) == BuildCharCounters(word2))
+	{
+		cout << word1 << " and " << word2 << " are anagrams" << endl;
+	}
+	else
+	{
+		cout << word1 << " and " << word2 << " are not anagrams" << endl;
+	}
+}
+
+int main()
+{
+	CountChars();
+	AnimalLegs();
+	Anagrams();
 }
