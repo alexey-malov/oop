70e72b5d4ed6951265b776e14232dc5737374063
Alexey Malov <malov.aleksey@gmail.com>
2021-05-13 20:44:24 +0300

    Обновлены материалы лекций

---
+++ labs/lab2/catch2_test/catch2_test.cpp
@@ -0,0 +1,15 @@
+﻿// catch2_test.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#define CATCH_CONFIG_MAIN
+#include "../../../catch2/catch.hpp"
+
+#include <iostream>
+#include <vector>
+
+TEST_CASE("vector comparison")
+{
+	using namespace Catch::literals;
+	std::vector<double> v1{1, 0.3, 3.5};	
+	std::vector<double> v2{ 1, 0.2 + 0.1, 3.5 };
+	CHECK_THAT(v1, Catch::Approx(v1));
+}
---
+++ samples/04-inheritance/class_internals_2/sandbox.cpp
@@ -0,0 +1,26 @@
+﻿#include "stdafx.h"
+#include <vector>
+#include <memory>
+
+using namespace std;
+
+class Shape
+{
+public:
+	virtual ~Shape() = default;
+};
+
+class Circle : public Shape
+{
+};
+class Rectangle : public Shape
+{
+};
+
+int main()
+{
+	vector<unique_ptr<Shape>> shapes;
+	shapes.emplace_back(make_unique<Circle>(10.0, Color::RED));
+	shapes.emplace_back(make_unique<Rectangle>(10.0, 20.0, Color::BLUE));
+	// Деструкторы unique_ptr корректно удалят фигуры через указатель на класс Shape
+}
