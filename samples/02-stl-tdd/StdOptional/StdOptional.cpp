f38cac24a166334fc5bc3e4c0eab1f07ed554e2c
Alexey Malov <malov.aleksey@gmail.com>
2023-04-10 10:49:29 +0300

    Обновил условия задач и пример с optional

--- samples/02-stl-tdd/StdOptional/StdOptional.cpp
+++ samples/02-stl-tdd/StdOptional/StdOptional.cpp
@@ -3,48 +3,54 @@
 
 using namespace std;
 
-enum class Color
-{
-	Red,
-	Yellow,
-	Green,
-	Black,
-	White
-};
+// clang-format off
+enum class Color { Red, Yellow, Green, Black, White };
+// clang-format on
 
-// Точка, у которой может быть или не быть задан цвет
 struct Point
 {
-	int x;
-	int y;
-	optional<Color> color;
+	double x, y;
 };
 
+struct Triangle
+{
+	Point vertex1, vertex2, vertex3;
+	optional<Color> outlineColor;
+	optional<Color> fillColor;
+};
+
+// clang-format off
 string ColorToString(Color color)
 {
 	switch (color)
 	{
-	case Color::Red:
-		return "red";
-	case Color::Yellow:
-		return "yellow";
-	case Color::Green:
-		return "green";
-	case Color::Black:
-		return "black";
-	case Color::White:
-		return "white";
+	case Color::Red: return "red";
+	case Color::Yellow: return "yellow";
+	case Color::Green: return "green";
+	case Color::Black: return "black";
+	case Color::White: return "white";
 	default:
 		assert(!"unknown color. please update code");
 		return "";
 	}
 }
+// clang-format on
 
-std::ostream& PrintPoint(ostream& strm, const Point& point)
+// Так в C++ переопределяется операция << для типов ostream и Point
+std::ostream& operator<<(ostream& strm, Point point)
 {
-	strm << "{" << point.x << ", " << point.y << ", ";
-	strm << (point.color ? ColorToString(point.color.value()) : "undefined color") << "}";
-	return strm;
+	return strm << "{" << point.x << ", " << point.y << "}";
+}
+
+ostream& operator<<(ostream& strm, const Triangle& t)
+{
+	strm << "{p1: " << t.vertex1 << ", p2: " << t.vertex2
+		 << ", p3: " << t.vertex3;
+	strm << ", outlineColor: "
+		 << (t.outlineColor ? ColorToString(*t.outlineColor) : "none"s);
+	strm << ", fillColor: "
+		 << (t.fillColor ? ColorToString(*t.fillColor) : "none"s);
+	return strm << "}";
 }
 
 struct Vector2d
@@ -61,7 +67,7 @@ struct LineSegment
 
 optional<Vector2d> IntersectLineSegements(const LineSegment& l1, const LineSegment& l2)
 {
-	optional<Vector2d> intersectionPoint; // по умолчанию optional хранит занчение "не инициализировано"
+	optional<Vector2d> intersectionPoint; // по умолчанию optional хранит аначение "не инициализировано"
 	if (true)
 	{
 		intersectionPoint = Vector2d{ 3.5, 2.8 };
@@ -72,8 +78,6 @@ optional<Vector2d> IntersectLineSegements(const LineSegment& l1, const LineSegme
 
 int main()
 {
-	Point point1{ 10, 20, nullopt };
-	Point point2{ 10, 20, Color::Red };
-	PrintPoint(cout, point1) << endl;
-	PrintPoint(cout, point2) << endl;
+	Triangle t{ { 0, 0 }, { 0, 1 }, { 1, 0 }, Color::Red, nullopt };
+	cout << t << endl;
 }
