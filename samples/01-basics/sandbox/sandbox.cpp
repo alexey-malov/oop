2ebc3a90d6ff22e64b3e38838a39392ccfeafee9
Alexey Malov <alexey.malov@ispringsolutions.com>
2024-03-29 18:51:36 +0300

    Обновлены примеры с копированием объектов

--- samples/01-basics/sandbox/sandbox.cpp
+++ samples/01-basics/sandbox/sandbox.cpp
@@ -1,321 +1,145 @@
-﻿#if 1
-#include <cassert>
-#include <fstream>
+﻿#include <fstream>
 #include <iostream>
-#include <latch>
-#include <mutex>
-#include <numbers>
-#include <sstream>
-#include <string>
 
-struct Point
-{
-	int x = 0;
-	int y = 0;
-};
-
-Point ReadPoint(std::istream& input)
-{
-	Point p;
-	input >> p.x >> p.y;
-	return p;
-}
-
-void WritePoint(std::ostream& output, Point p)
-{
-	output << p.x << " " << p.y;
-}
-namespace ver1
-{
-
-class Time
-{
-	int m_hours = 0;
-	int m_minutes = 0;
-	int m_seconds = 0;
-};
-
-} // namespace ver1
-
-namespace ver2
-{
-
-class Time
-{
-	int m_secondsSinceMidnight = 0;
-};
+//using namespace std;
 
 #if 0
-void Test()
-{
-	Time t;
-	t.m_secondsSinceMidnight = 42;
-	// error: cannot access private member declared in class 'Time'
+int main(int argc, char* argv[]) {
+	if (argc != 3) { // Указаны ли верно параметры командной строки?
+		std::cout << "Invalid arguments count\n"
+			 << "Usage: copyfile.exe <input file> <output file>\n";
+		return 1;
+	}
+	std::ifstream input(argv[1]);
+	if (!input.is_open()) { // Удалось ли открыть входной файл?
+		std::cout << "Failed to open " << argv[1] << " for reading\n";
+		return 1;
+	}
+	std::ofstream output(argv[2]);
+	if (!output.is_open()) { // Удалось ли открыть выходной файл
+		std::cout << "Failed to open " << argv[2] << " for writing\n";
+		return 1;
+	}
+	for (char ch; input.get(ch);) { // Читаем посимвольно данные
+		if (!output.put(ch)) {      // и записываем в выходной файл
+			std::cout << "Failed to save data on disk\n";
+			return 1;
+		}
+	}
+	if (!input.eof()) { // Все ли данные были считаны?
+		std::cout << "Failed to read input file\n";
+		return 1;
+	}
+	if (!output.flush()) { // Успешно ли данные были записаны?
+		std::cout << "Failed to save data on disk\n";
+		return 1;
+	}
+	return 0; // Всё ок, завершаем программу и сообщаем об успехе
 }
 #endif
 
-} // namespace ver2
-
-namespace ver3
-{
-// Структура глазами компилятора
-struct Time
-{
-	// Этот спецификатор public неявно подразумевается внутри struct.
-public:
-	// Всё, что ниже public:, доступно для внешнего кода.
-	int hours = 0;
-	int minutes = 0;
-	int seconds = 0;
-};
-} // namespace ver3
-
-namespace ver4
-{
-// Класс глазами компилятора
-class Time
-{
-	// Этот спецификатор private неявно подразумевается внутри class.
-private:
-	// Всё, что ниже private:, скрыто от внешнего мира.
-	int m_hours = 0;
-	int m_minutes = 0;
-	int m_seconds = 0;
-};
-} // namespace ver4
-
-namespace ver5
-{
-class Time
-{
-public:
-	int GetHours() { return m_secondsSinceMidnight / 3600; }
-	int GetMinutes() { return (m_secondsSinceMidnight / 60) % 60; }
-	int GetSeconds() { return m_secondsSinceMidnight % 60; }
-
-	bool SetTime(int hours, int minutes, int seconds)
-	{
-		if (hours < 0 || hours >= 24 || minutes < 0 || minutes >= 60 || seconds < 0 || seconds >= 60)
-		{
+#if 0
+bool CopyFile(std::string inputName, std::string outputName, std::string& error) {
+	std::ifstream input(inputName);
+	if (!input.is_open()) { // Удалось ли открыть входной файл?
+		error = "Failed to open " + inputName + " for reading";
+		return false;
+	}
+	std::ofstream output(outputName);
+	if (!output.is_open()) { // Удалось ли открыть выходной файл
+		error = "Failed to open " + outputName + " for writing";
+		return false;
+	}
+	for (char ch; input.get(ch);) { // Читаем посимвольно данные
+		if (!output.put(ch)) {      // и записываем в выходной файл
+			error = "Failed to save data on disk";
 			return false;
 		}
-
-		m_secondsSinceMidnight = hours * 60 * 60 + minutes * 60 + seconds;
-
-		return true;
 	}
-
-private:
-	int m_secondsSinceMidnight = 0;
-};
-} // namespace ver5
-
-namespace
-{
-class SomeClass
-{
-public:
-	// Это метод класса. Выглядит как функция, описанная внутри класса.
-	int GetData()
-	{
-		// К приватным данным можно обращаться только внутри методов класса.
-		return m_data;
+	if (!input.eof()) { // Все ли данные были считаны?
+		error = "Failed to read input file";
+		return false;
 	}
-
-private:
-	// Данные надёжно спрятаны от внешнего мира.
-	// Никто, кроме самого класса, не имеет к ним доступа.
-	int m_data = 42;
-};
-} // namespace
-
-namespace geom2
-{
-
-class Circle
-{
-public:
-	void SetRadius(double r)
-	{
-		m_radius = r;
+	if (!output.flush()) { // Успешно ли данные были записаны?
+		error = "Failed to save data on disk";
+		return false;
 	}
-	double GetRadius() const { return m_radius; }
-	double GetArea() const { return std::numbers::pi * m_radius * m_radius; }
-
-private:
-	double m_radius;
-};
-
-void PrintCircle(const Circle& c)
-{
-	std::cout << "radius=" << c.GetRadius() << ", area=" << c.GetArea() << std::endl;
+	return true;
 }
 
-void main()
-{
-	Circle c;
-	c.SetRadius(10);
-	PrintCircle(c);
-}
-} // namespace geom2
-
-namespace mutmut
-{
-class ThreadsafeCounter {
-public:
-	int Get() const {
-		std::lock_guard lk(m_mutex);
-		return m_data;
+int main(int argc, char* argv[]) {
+	if (argc != 3) { // Указаны ли верно параметры командной строки?
+		std::cout << "Invalid arguments count\n"
+			 << "Usage: copyfile.exe <input file> <output file>\n";
+		return 1;
 	}
-	void Inc() {
-		std::lock_guard lk(m_mutex);
-		++m_data;
-	}
-private:
-	// The "M&M rule": mutable and mutex go together
-	mutable std::mutex m_mutex;
-	int m_data = 0;
-};
-
-class NonThreadsafeCounter
-{
-public:
-	int Get() const
+	if (std::string error; !CopyFile(argv[1], argv[2], error))
 	{
-		return m_data;
+		std::cout << error << std::endl;
 	}
-	void Inc()
-	{
-		++m_data;
-	}
-
-private:
-	int m_data = 0;
-};
-} // namespace mutmut
-
-int main()
-{
-	using namespace mutmut;
-
-	ThreadsafeCounter counter;
-
-	{
-		// Триггер, который разблокируется, когда трижды вызовут arrive_and_wait
-		std::latch l(3);
-		std::jthread t1([&counter, &l] {
-			l.arrive_and_wait();
-			for (int i = 0; i < 1'000'000; ++i)
-			{
-				counter.Inc();
-			}
-		});
-		std::jthread t2([&counter, &l] {
-			l.arrive_and_wait();
-			for (int i = 0; i < 1'000'000; ++i)
-			{
-				counter.Inc();
-			}
-		});
-		std::jthread t3([&counter, &l] {
-			l.arrive_and_wait();
-			for (int i = 0; i < 100; ++i)
-			{
-				std::cout << counter.Get() << std::endl;
-			}
-		});
-	} // Все потоки завершат работу при выходе из блока
-	std::cout << "---" << std::endl;
-	std::cout << counter.Get() << std::endl;
-}
-
-#else
-#include <iostream>
-
-class Mutable
-{
-public:
-	int GetValue() const
-	{
-		if (m_value == -1)
-		{
-			m_value = 42;
-		}
-		return m_value;
-	}
-	int GetCounter() const { return m_counter; }
-	void Change()
-	{
-		std::cout << "Enter Immutable::Change" << std::endl;
-		++m_counter;
-		std::cout << "Exit Immutable::Change" << std::endl;
-	}
-
-private:
-	mutable int m_value = -1;
-	int m_counter = 0;
-};
-
-class Immutable
-{
-public:
-	int GetValue() const
-	{
-		return m_value;
-	}
-	int GetCounter() const { return m_counter; }
-	void Change()
-	{
-		std::cout << "Enter Immutable::Change" << std::endl;
-		++m_counter;
-		std::cout << "Exit Immutable::Change" << std::endl;
-	}
-
-private:
-	int m_value = 42;
-	int m_counter = 0;
-};
-
-const Mutable mut;
-const Immutable immut;
-
-int main()
-{
-	std::cout << "Immutable: " << &immut << " - " << immut.GetValue() << std::endl;
-	std::cout << "  Mutable: " << &mut << " - " << mut.GetValue() << std::endl;
-
-	const_cast<Mutable&>(mut).Change();
-	std::cout << mut.GetCounter() << std::endl;
-
-	const_cast<Immutable&>(immut).Change();
-	std::cout << immut.GetCounter() << std::endl;
+	return 0;
 }
 #endif
 
-struct Vector {
-    double x = 0, y = 0;
-};
-
-struct Point {
-    double x = 0, y = 0;
-
-    void Set(double x, double y) {
-        // Область видимости параметров x и y ограничена 
-        // методом Point::Set. Поэтому внутри метода Set 
-        // имена x и y относятся к параметрам, а не к полям.
-
-        // Когда мы пишем this->x и this->y, то явно сообщаем 
-        // компилятору, что обращаемся к полям текущего объекта.
-        this->x = x;
-        this->y = y;
-
-        // Если напишем x = x, то присвоим параметру x его собственное значение.
-    }
-};
-int main() {
-    Point p1, p2;
-
-    // Для вызова метода указываем объект.
-    p1.MoveBy({1, 2.5});
-    p2.MovePoint({-1, -1});
+#if 1
+bool CopyStream(std::ifstream& input, std::ofstream& output, std::string& error) {
+	for (char ch; input.get(ch);) { // Читаем посимвольно данные
+		if (!output.put(ch)) {      // и записываем в выходной файл
+			error = "Failed to save data on disk";
+			return false;
+		}
+	}
+	if (!input.eof()) { // Все ли данные были считаны?
+		error = "Failed to read input file";
+		return false;
+	}
+	if (!output.flush()) { // Успешно ли данные были записаны?
+		error = "Failed to save data on disk";
+		return false;
+	}
+	return true;
 }
+
+bool CopyFile(std::string inputName, std::string outputName, std::string& error) {
+	std::ifstream input(inputName);
+	if (!input.is_open()) { // Удалось ли открыть входной файл?
+		error = "Failed to open " + inputName + " for reading";
+		return false;
+	}
+	std::ofstream output(outputName);
+	if (!output.is_open()) { // Удалось ли открыть выходной файл
+		error = "Failed to open " + outputName + " for writing";
+		return false;
+	}
+	return CopyStream(input, output, error);
+}
+
+// Переводит время в строку
+std::string TimeToString(Time t) { ... }
+
+// Переводит время в строку в формате "hh:mm:ss"
+std::string TimeToString(Time t) { ... }
+
+
+int GenerateRandomNumber() {
+	// TODO: Генератор случайных числе будет доработан в задаче TASK-888
+	return 42;
+}
+
+void OnClick()
+{
+}
+
+int main(int argc, char* argv[]) {
+	if (argc != 3) { // Указаны ли верно параметры командной строки?
+		std::cout << "Invalid arguments count\n"
+			 << "Usage: copyfile.exe <input file> <output file>\n";
+		return 1;
+	}
+	if (std::string error; !CopyFile(argv[1], argv[2], error))
+	{
+		std::cout << error << std::endl;
+	}
+	return 0;
+}
+#endif
---
+++ samples/03-classes/copying/copying.cpp
@@ -0,0 +1,62 @@
+﻿// copying.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+
+class PostCard
+{
+public:
+	PostCard(const std::string& text)
+		: m_text{ text }
+	{
+		std::cout << "Post card was created: " << m_text << std::endl;
+	}
+	// Копирующий конструктор
+	PostCard(const PostCard& other)
+		: m_text{ other.m_text }
+	{
+		std::cout << "Post card is copied: " << m_text << std::endl;
+	}
+	~PostCard() { std::cout << "Post card was destroyed: " << m_text << std::endl; }
+
+	const std::string& GetText() const { return m_text; }
+
+	void SetText(const std::string& text) { m_text = text; }
+
+private:
+	std::string m_text;
+};
+
+PostCard MakeModifiedPostCard(PostCard postCard) {
+	std::cout << "Enter MakeModifiedPostCard" << std::endl;
+	postCard.SetText(postCard.GetText() + " - modified");
+	std::cout << "Exit MakeModifiedPostCard" << std::endl;
+	return postCard;
+}
+
+struct Envelope
+{
+	PostCard postCard;
+};
+
+Envelope MakeModifiedPostcardInEnvelope(Envelope envelope)
+{
+	std::cout << "Enter MakeModifiedPostCard" << std::endl;
+	envelope.postCard.SetText(envelope.postCard.GetText() + " - modified");
+	std::cout << "Exit MakeModifiedPostCard" << std::endl;
+	return envelope;
+}
+
+int main()
+{
+#if 0
+	PostCard postCard{ "Hello" };
+	auto modifiedPostCard = MakeModifiedPostCard(postCard);
+	std::cout << modifiedPostCard.GetText() << std::endl;
+#endif
+
+	Envelope env{.postCard = "Hello" };
+	auto modifiedEnv = MakeModifiedPostcardInEnvelope(env);
+	std::cout << modifiedEnv.postCard.GetText() << std::endl;
+}
