5a7c5449649801b94b571426ba1e85d2aa77e2d2
Alexey Malov <malov.aleksey@gmail.com>
2024-04-27 11:51:12 +0300

    Доработал пример про shared_ptr

--- samples/01-basics/sharedptr/main.cpp
+++ samples/01-basics/sharedptr/main.cpp
@@ -55,10 +55,87 @@ void WrapExistingObjectOnStackIntoSharedPtr()
 	// в котором объект был когда-то
 	// outerSp->DoSomething();
 }
+struct Slave;
+struct Task
+{
+	int id;
+};
 
+struct Master
+{
+	Master()
+	{
+		std::cout << "Master is created" << std::endl;
+	}
+	~Master()
+	{
+		std::cout << "Master is destroyed" << std::endl;
+	}
+
+	Task GetTask()
+	{
+		return { m_nextTaskId++ };
+	}
+
+	std::shared_ptr<Slave> slave1;
+	std::shared_ptr<Slave> slave2;
+
+private:
+	int m_nextTaskId = 0;
+};
+
+struct Slave
+{
+	Slave()
+	{
+		std::cout << "Slave is created" << std::endl;
+	}
+	~Slave()
+	{
+		std::cout << "Slave is destroyed" << std::endl;
+	}
+
+	void Work()
+	{
+		if (auto m = master.lock())
+		{
+			auto task = m->GetTask();
+			std::cout << "Slave performs task #" << task.id << std::endl;
+		}
+		else
+		{
+			std::cout << "Slave has no master" << std::endl;
+		}
+	}
+
+	// Slave не владеет объектом Master, а хранит слабую ссылку на него.
+	std::weak_ptr<Master> master;
+};
+
+void TestWeakPtr()
+{
+	auto master = std::make_shared<Master>();
+	auto slave1 = std::make_shared<Slave>();
+	auto slave2 = std::make_shared<Slave>();
+
+	master->slave1 = slave1;
+	slave1->master = master;
+
+	master->slave2 = slave2;
+	slave2->master = master;
+	
+	slave1.reset();
+	slave2->Work();
+	slave2->Work();
+	master.reset();
+	slave2->Work();
+	std::cout << "Exit from function" << std::endl;
+}
 
 int main()
 {
+	TestWeakPtr();
+	std::cout << "---" << std::endl;
 	CreateNewObjectAndWrapItIntoSharedPtr();
 	WrapExistingObjectInHeapIntoSharedPtr();
 }
